61e6a2d44bd95eff0124a40c2b264dc19483a143
Luiz Henrique Vitorino <luizvitorinohenrique@gmail.com>
2023-04-23 18:04:21 -0300

    feat: event click remve edit and done

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,7 @@ const saveTodo = (text) => {
   todoInput.value = "";
   todoInput.focus();
 };
+
 //Eventos
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -43,5 +44,21 @@ todoForm.addEventListener("submit", (e) => {
 
   if (inputValue) {
     saveTodo(inputValue);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+
+  if (targetEl.classList.contains("finish-todo")) {
+    parentEl.classList.toggle("done");
+  }
+
+  if (targetEl.classList.contains("remove-todo")) {
+    parentEl.remove();
+  }
+
+  if (targetEl.classList.contains("edit-todo")) {
   }
 });
